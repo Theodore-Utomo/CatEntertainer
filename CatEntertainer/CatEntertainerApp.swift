d216786aa8d79c8943daa64c6692fc8e4502bc3a
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-16 22:35:24 -0400

    Initial Commit

---
+++ CatEntertainer/CatEntertainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatEntertainerApp.swift
+//  CatEntertainer
+//
+//  Created by Theodore Utomo on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CatEntertainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatEntertainerTests/CatEntertainerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatEntertainerTests.swift
+//  CatEntertainerTests
+//
+//  Created by Theodore Utomo on 9/16/24.
+//
+
+import Testing
+@testable import CatEntertainer
+
+struct CatEntertainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
